61d2f5b8d99bc39044904e3b770f21cfa11b3940
HoangPham1208 <hoang.pham1208@hcmut.edu.vn>
2024-07-01 17:55:39 +0700

    test for slashing + unstaking, use ur wif

--- lib/library-starter/src/taproot/bip341.ts
+++ lib/library-starter/src/taproot/bip341.ts
@@ -1,4 +1,4 @@
-export const NUMS =
+export const NUMS: string =
   "022ae24aecee27d2f6b4c80836dfe1e86a6f9a14a4dd3b1d269bdeda4e6834e82f";
 // "0250929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0";
-export const LEAF_VERSION_TAPSCRIPT = 0xc0;
+export const LEAF_VERSION_TAPSCRIPT: number = 0xc0;
